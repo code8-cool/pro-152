9f4a5e801d06887901d9d3cc4e4df40a9ff8a753
code8-cool <136864069+code8-cool@users.noreply.github.com>
2024-07-22 22:04:23 -0700

    Add files via upload

---
+++ c-152 act 1d 2d 3d start.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 22 19:17:32 2024
+
+@author: occup
+"""
+
+list_1d = ["grade 4", "grade 5", "grade 6", "grade 7"]
+
+#print(list_1d)
+
+#print(list_1d[3])
+
+list_2d = ["grade 4", "grade 5", "grade 6", ["1", "10", "100", ]]
+
+#print(list_2d[3])
+#print(list_2d[1])
+#print(list_2d[3][2])
+
+list_3d = [  [ [ "Steve", "B+", "82%", "2nd position" ], ["Brian", "A+", "99%", "1st position"], ["evene", "c+", "70%", "3rd position"  ] ] ]
+
+print(list_3d[0][0][0])
+print(list_3d[0][0][1])
+print(list_3d[0][0][2])
+print(list_3d[0][0][3])
+
+print(list_3d[0][1][0])
+print(list_3d[0][1][1])
+print(list_3d[0][1][2])
+print(list_3d[0][1][3])
+
+print(list_3d[0][2][0])
+print(list_3d[0][2][1])
+print(list_3d[0][2][2])
+print(list_3d[0][2][3])
+
+
+
